ec64fc1ca4ce58f6caaf387d783078032367a6fa
Nahartor <nicolabalbonipalma@gmail.com>
2023-06-09 19:30:41 +0200

    Update Repository 09/06/2023 3

--- install_docker_and_dependences.sh
+++ install_docker_and_dependences.sh
@@ -18,27 +18,26 @@ apt install docker-compose -y
 
 echo "Installation completed."
 
-'
-A continuación podrá encontrar el código en español. Si desea usar este script en español solo tiene que borrar todo entre la primera línea del texto y la linea 23 (y también la linea 44). 
-# Verificar si se está ejecutando como superusuario
-if [ "$(id -u)" -ne 0 ]; then
-  echo "Este script debe ser ejecutado con permisos de superusuario." >&2
-  exit 1
-fi
+
+# A continuación podrá encontrar el código en español. Si desea usar este script en español solo tiene que borrar todo entre la primera línea del texto y la linea 23 (y los caracteres # delante de cada linea restante). 
+# # Verificar si se está ejecutando como superusuario
+# if [ "$(id -u)" -ne 0 ]; then
+#   echo "Este script debe ser ejecutado con permisos de superusuario." >&2
+#   exit 1
+# fi
 
 
-# Actualizar y actualizar el sistema
-apt update
-apt upgrade -y
+# # Actualizar y actualizar el sistema
+# apt update
+# apt upgrade -y
 
 
-# Instalar Docker
-apt install docker.io -y
+# # Instalar Docker
+# apt install docker.io -y
 
 
-# Instalar Docker Compose
-apt install docker-compose -y
+# # Instalar Docker Compose
+# apt install docker-compose -y
 
 
-echo "Instalación completada."
-'
+# echo "Instalación completada."
--- menu_docker.sh
+++ menu_docker.sh
@@ -54,59 +54,58 @@ while true; do
   echo
 done
 
-'
-A continuación podrá encontrar el código en español. Si desea usar este script en español solo tiene que borrar todo entre la primera línea del texto y la linea 59 (y también la linea 112). 
-# Verificar si se está ejecutando como superusuario
-if [ "$(id -u)" -ne 0 ]; then
-  echo "Este script debe ser ejecutado con permisos de superusuario." >&2
-  exit 1
-fi
 
-# Función para levantar todos los contenedores
-levantar_contenedores() {
-  docker-compose up -d
-}
+# A continuación podrá encontrar el código en español. Si desea usar este script en español solo tiene que borrar todo entre la primera línea del texto y la linea 59 (y los caracteres # delante de cada linea restante). 
+# # Verificar si se está ejecutando como superusuario
+# if [ "$(id -u)" -ne 0 ]; then
+#   echo "Este script debe ser ejecutado con permisos de superusuario." >&2
+#   exit 1
+# fi
 
-# Función para parar todos los contenedores
-parar_contenedores() {
-  docker-compose down
-}
+# # Función para levantar todos los contenedores
+# levantar_contenedores() {
+#   docker-compose up -d
+# }
 
-# Función para parar un contenedor específico
-parar_contenedor_especifico() {
-  read -p "Ingrese el nombre del contenedor que desea parar (wordpress, db, php): " contenedor
-  docker-compose stop $contenedor
-}
+# # Función para parar todos los contenedores
+# parar_contenedores() {
+#   docker-compose down
+# }
 
-# Función para levantar un contenedor específico
-levantar_contenedor_especifico() {
-  read -p "Ingrese el nombre del contenedor que desea levantar (wordpress, db, php): " contenedor
-  docker-compose up -d $contenedor
-}
+# # Función para parar un contenedor específico
+# parar_contenedor_especifico() {
+#   read -p "Ingrese el nombre del contenedor que desea parar (wordpress, db, php): " contenedor
+#   docker-compose stop $contenedor
+# }
 
-# Menú principal
-while true; do
-  echo "==== Menu ===="
-  echo "1. Levantar todos los contenedores"
-  echo "2. Parar todos los contenedores"
-  echo "3. Parar un contenedor específico"
-  echo "4. Levantar un contenedor específico"
-  echo "5. Ver los registros de los contenedores"
-  echo "6. Salir"
-  echo "=============="
+# # Función para levantar un contenedor específico
+# levantar_contenedor_especifico() {
+#   read -p "Ingrese el nombre del contenedor que desea levantar (wordpress, db, php): " contenedor
+#   docker-compose up -d $contenedor
+# }
 
-  read -p "Seleccione una opción: " opcion
+# # Menú principal
+# while true; do
+#   echo "==== Menu ===="
+#   echo "1. Levantar todos los contenedores"
+#   echo "2. Parar todos los contenedores"
+#   echo "3. Parar un contenedor específico"
+#   echo "4. Levantar un contenedor específico"
+#   echo "5. Ver los registros de los contenedores"
+#   echo "6. Salir"
+#   echo "=============="
 
-  case $opcion in
-    1) levantar_contenedores ;;
-    2) parar_contenedores ;;
-    3) parar_contenedor_especifico ;;
-    4) levantar_contenedor_especifico ;;
-    5) docker-compose logs ;;
-    6) exit ;;
-    *) echo "Opción inválida. Por favor, seleccione una opción válida." ;;
-  esac
+#   read -p "Seleccione una opción: " opcion
 
-  echo
-done
-'
+#   case $opcion in
+#     1) levantar_contenedores ;;
+#     2) parar_contenedores ;;
+#     3) parar_contenedor_especifico ;;
+#     4) levantar_contenedor_especifico ;;
+#     5) docker-compose logs ;;
+#     6) exit ;;
+#     *) echo "Opción inválida. Por favor, seleccione una opción válida." ;;
+#   esac
+
+#   echo
+# done
